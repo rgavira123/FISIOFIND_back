a4df5f8b449405d423c354006fa1e81508e94aaa
MiguelEncina <migencmar@alum.us.es>
2025-03-03 18:21:02 +0100

    feat: añadidos datos de prueba de citas

--- backend/fisio_find/gestion_citas/views.py
+++ backend/fisio_find/gestion_citas/views.py
@@ -6,7 +6,7 @@ from gestion_citas.serializers import AppointmentSerializer
 from rest_framework.permissions import IsAuthenticated
 
 class AppointmentList(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
@@ -14,6 +14,6 @@ class AppointmentList(generics.ListCreateAPIView):
     ordering_fields = ['start_time', 'end_time']
 
 class AppointmentDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated, IsOwner]
+    # permission_classes = [IsAuthenticated, IsOwner]
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
