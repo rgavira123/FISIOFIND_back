651158a76749a45d1dc2af40a40e873cb7fce749
MiguelEncina <migencmar@alum.us.es>
2025-03-03 18:20:38 +0100

    fix: cambiar url de citas para que siga la estructura api/*

--- backend/fisio_find/fisio_find/urls.py
+++ backend/fisio_find/fisio_find/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app_user/', include('gestion_usuarios.urls')),
-    path('app_appointment/', include('gestion_citas.urls')),
+    path('api/', include('gestion_citas.urls')),
 ]
