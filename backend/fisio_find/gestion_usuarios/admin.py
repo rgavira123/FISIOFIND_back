4d7e49a7744b9ac44f6848d00ba4e808d01b9858
Francisco Mateos Villarejo <framatvil@alum.us.es>
2025-03-10 17:19:56 +0100

    feat: Seeders de Appointment

--- backend/fisio_find/gestion_usuarios/admin.py
+++ backend/fisio_find/gestion_usuarios/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import AppUser
+from .models import AppUser, Patient, Physiotherapist, Admin
 
 # Register your models here.
 
 admin.site.register(AppUser)
+admin.site.register(Patient)
+admin.site.register(Physiotherapist)
+admin.site.register(Admin)
