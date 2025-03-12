0e1bde50e429ea21e58428fd97c0519c9d36c44a
guaridpin <guaridpin@alum.us.es>
2025-03-12 10:01:46 +0100

    fix: arreglada creación de nuevas especialidades

--- backend/fisio_find/gestion_usuarios/serializers.py
+++ backend/fisio_find/gestion_usuarios/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.password_validation import validate_password
 from django.db.utils import IntegrityError
 from django.db import transaction
 from gestion_usuarios.validacionFisios import validar_colegiacion
-from .models import AppUser, Patient, Physiotherapist, Specialization
+from .models import AppUser, Patient, Physiotherapist, PhysiotherapistSpecialization, Specialization
 import re
 from datetime import date  # Importar para obtener la fecha de hoy
 
@@ -153,12 +153,16 @@ class PhysioRegisterSerializer(serializers.ModelSerializer):
     last_name = serializers.CharField(required=True)
     phone_number = serializers.CharField(required=True)
     postal_code = serializers.CharField(required=True)
+    specializations = serializers.ListField(
+        child=serializers.CharField(), required=False  # Lista de nombres de especializaciones
+    )
     
     class Meta:
         model = Physiotherapist
         fields = [
             'username', 'email', 'password', 'dni', 'gender', 'first_name', 'last_name', 
-            'birth_date', 'collegiate_number', 'autonomic_community', 'phone_number', 'postal_code'
+            'birth_date', 'collegiate_number', 'autonomic_community', 'phone_number', 'postal_code',
+            'specializations'
         ]
         
     def validate_password(self, value):
@@ -190,6 +194,9 @@ class PhysioRegisterSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         """Manejo de IntegrityError con transactions para asegurar rollback en caso de fallo."""
+        
+        specializations_data = validated_data.pop('specializations', [])
+        
         try:
             with transaction.atomic():
                 first_name = validated_data.pop('first_name')
@@ -226,6 +233,11 @@ class PhysioRegisterSerializer(serializers.ModelSerializer):
                     collegiate_number=collegiate_number,
                     gender=gender
                 )
+                
+                # Manejar especializaciones
+                for spec_name in specializations_data:
+                    specialization, created = Specialization.objects.get_or_create(name=spec_name)
+                    PhysiotherapistSpecialization.objects.create(physiotherapist=physio, specialization=specialization)
                 
                 return physio
 
