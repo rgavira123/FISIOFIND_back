69b5a9e82dd91a51050bdfd590753aa3e326eeee
Benjamín Maureira Flores <benmauflo@alum.us.es>
2025-03-12 12:48:26 +0100

    Revert "Merge branch 'feat/perfil-fisio' of https://github.com/Proyecto-ISPP/ISPP into feat/perfil-fisio"
    This reverts commit 91551a6324a529aac5a6d48d0362ac3ac4f3f23d, reversing
    changes made to c263c6c458c971cca11d76ca45c3c80cd4cfb039.

--- backend/fisio_find/fisio_find/urls.py
+++ backend/fisio_find/fisio_find/urls.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +7,3 @@ urlpatterns = [
     path('api/app_appointment/', include('gestion_citas.urls')),
     path('api/terminos/', include('gestion_terminos.urls')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
