81739f10f1cf6f9ff662063c9a1c989e6bfd598e
Benjamín Maureira Flores <benmauflo@alum.us.es>
2025-03-12 20:15:29 +0100

    fix: guardado de imagenes

--- backend/fisio_find/gestion_usuarios/urls.py
+++ backend/fisio_find/gestion_usuarios/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path('current-user/', return_user, name='current_user')
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
 
