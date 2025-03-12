be524f9a68651b3e1801656091132473afbb688e
Rafa <rafpulcif@alum.us.es>
2025-03-12 22:04:53 +0100

    feat: añadidos mas hosts permitidos

--- backend/fisio_find/fisio_find/settings.py
+++ backend/fisio_find/fisio_find/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'fisiofind-backend.azurewebsites.net']
 
 
 # Application definition
