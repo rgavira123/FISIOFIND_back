26668e6868f8a0f1f65cc5ad42fb4487166e8f4e
Ramón Gavira <91947011+rgavira123@users.noreply.github.com>
2025-03-18 20:47:59 +0100

    Update settings.py

--- backend/fisio_find/fisio_find/settings.py
+++ backend/fisio_find/fisio_find/settings.py
@@ -197,6 +197,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "/root/FISIOFIND_back/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
