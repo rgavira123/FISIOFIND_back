0ea5d2c4c1243d923f543c763e6af686bb19e68f
Daniel Ruiz López <danruilop1@alum.us.es>
2025-03-19 13:12:54 +0100

    fix: sidebar arreglada

--- frontend/components/sidebar-demo.tsx
+++ frontend/components/sidebar-demo.tsx
@@ -91,7 +91,7 @@ export function SidebarDemo() {
   return (
     <div
       className={cn(
-        "fixed left-0 top-0 flex flex-col md:flex-row bg-gray-50 dark:bg-neutral-900 h-screen w-[60px] border-r border-gray-200 dark:border-neutral-800"
+        "fixed left-0 top-0 flex flex-col md:flex-row bg-gray-50 dark:bg-neutral-900 h-screen w-[80px] border-r border-gray-200 dark:border-neutral-800 z-50"
       )}
     >
       <Sidebar open={open} setOpen={setOpen}>
@@ -136,14 +136,12 @@ const Logo = () => {
       <img
         src="/static/fisio_find_logo.webp"
         alt="Logo"
-        className="h-1.5 w-auto flex-shrink-0"
+        className="h-8 w-auto flex-shrink-0"
         style={{ filter: "brightness(0) invert(0)" }}
       />
     </a>
   );
 };
-
-export default Logo;
 
 export const LogoIcon = () => {
   return (
@@ -154,7 +152,7 @@ export const LogoIcon = () => {
       <img
         src="/static/fisio_find_logo.webp"
         alt="Fisio Find logo"
-        className="h-0.5 w-auto flex-shrink-0"
+        className="h-6 w-auto flex-shrink-0"
         style={{ filter: "brightness(0) invert(0)" }}
       />
     </a>
