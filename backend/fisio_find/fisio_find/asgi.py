ff8f634c8d61caf26209c83a0470adfb60ae7b78
Ramón Gavira <91947011+rgavira123@users.noreply.github.com>
2025-03-20 19:10:55 +0100

    Update asgi.py

--- backend/fisio_find/fisio_find/asgi.py
+++ backend/fisio_find/fisio_find/asgi.py
@@ -1,19 +1,14 @@
-# asgi.py
-
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.urls import path
-from videocall import consumers
+from videocall.routing import websocket_urlpatterns  # Importamos las rutas WebSocket
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter([
-            path("ws/room/<room_code>/", consumers.ChatConsumer.as_asgi()),  # Configuración de WebSocket
-        ])
+        URLRouter(websocket_urlpatterns)
     ),
 })
