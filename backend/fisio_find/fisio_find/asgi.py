19f0142b796fb32923ceae6039df85ae9611adaa
Ramón Gavira <91947011+rgavira123@users.noreply.github.com>
2025-03-20 19:31:46 +0100

    Update asgi.py

--- backend/fisio_find/fisio_find/asgi.py
+++ backend/fisio_find/fisio_find/asgi.py
@@ -5,7 +5,7 @@ from channels.auth import AuthMiddlewareStack
 from django.urls import path
 from videocall import consumers  # Asegúrate de que está correcto
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fisio_find.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
