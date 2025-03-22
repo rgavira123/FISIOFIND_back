75c16279427fbfb5bced623b4629b19b482b8ee4
Antonio Macías <antmacfer1@alum.us.es>
2025-03-22 19:33:10 +0100

    feat: completado pagina de terminos

--- backend/fisio_find/terms/apps.py
+++ backend/fisio_find/terms/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class GestionTerminosConfig(AppConfig):
+class TermsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gestion_terminos'
+    name = 'terms'
