66e9f799c2c17a49163fec1b24a0e0ab7dd346d9
Ramón Gavira <91947011+rgavira123@users.noreply.github.com>
2025-03-26 22:08:42 +0100

    feat: borrar gestion pagos

--- backend/fisio_find/gestion_pagos/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Payment
-
-class PaymentSerializer(serializers.ModelSerializer):
-    payment_deadline = serializers.DateTimeField(read_only=True)
-
-    class Meta:
-        model = Payment
-        fields = ['id', 'appointment', 'amount', 'payment_method', 'status', 'payment_date', 'payment_deadline']
