8a722cd3b8e947ea57d12537ee47016fac9f30ab
Ramón Gavira <91947011+rgavira123@users.noreply.github.com>
2025-03-27 17:55:40 +0100

    Update admin.py

--- backend/fisio_find/payment/admin.py
+++ backend/fisio_find/payment/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Payment
 
-admin.site.register(Payment)
+
+class PaymentsAdmin(admin.ModelAdmin):
+    search_fields = [
+        'appointment__patient__user__email','appointment__patient__user__username','appointment__patient__user__first_name','appointment__patient__user__last_name','appointment__patient__user__dni',
+        'appointment__physiotherapist__user__email','appointment__physiotherapist__user__username','appointment__physiotherapist__user__first_name','appointment__physiotherapist__user__last_name','appointment__physiotherapist__user__dni',
+        'amount',
+        ]
+    list_filter = ['payment_method', 'status','payment_date']
+    list_display = ['id','appointment__patient__user__username', 'appointment__physiotherapist__user__username', 'payment_method','status']
+
+
+admin.site.register(Payment, PaymentsAdmin)
