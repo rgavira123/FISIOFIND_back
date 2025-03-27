b886df221f822e176c13fedf6f31e3890559121c
Ramón Gavira <91947011+rgavira123@users.noreply.github.com>
2025-03-27 17:56:03 +0100

    Update admin.py

--- backend/fisio_find/treatments/admin.py
+++ backend/fisio_find/treatments/admin.py
@@ -19,8 +19,26 @@ class TreatmentAdmin(admin.ModelAdmin):
     get_patient_name.short_description = 'Patient'
     get_physiotherapist_name.short_description = 'Physiotherapist'
     
-admin.site.register(Session)
-admin.site.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    search_fields = [
+        'physiotherapist__user__email','physiotherapist__user__username','physiotherapist__user__first_name','physiotherapist__user__last_name','physiotherapist__user__dni',
+        'title', 'description',
+        ]
+    list_filter = ['area']
+    list_display = ['id','title', 'physiotherapist__user__username', 'area']
+
+class SessionAdmin(admin.ModelAdmin):
+    search_fields = [
+        'treatment__physiotherapist__user__email','treatment__physiotherapist__user__username','treatment__physiotherapist__user__first_name','treatment__physiotherapist__user__last_name','treatment__physiotherapist__user__dni',
+        'treatment__patient__user__email','treatment__patient__user__username','treatment__patient__user__first_name','treatment__patient__user__last_name','treatment__patient__user__dni',
+        'name', 'day_of_week'
+        ]
+    list_display = ['id','name', 'treatment__physiotherapist__user__username', 'treatment__patient__user__username', 'day_of_week']
+
+
+    
+admin.site.register(Session, SessionAdmin)
+admin.site.register(Exercise, ExerciseAdmin)
 admin.site.register(ExerciseSession)
 admin.site.register(Series)
 admin.site.register(ExerciseLog)
