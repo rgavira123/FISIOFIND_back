92bbdadcc58e15f473fed3098ae4d4fb9185f5dd
Ramón Gavira <91947011+rgavira123@users.noreply.github.com>
2025-03-27 15:51:25 +0100

    Update admin.py

--- backend/fisio_find/users/admin.py
+++ backend/fisio_find/users/admin.py
@@ -8,7 +8,7 @@ class AppUserAdmin(BaseUserAdmin):
     form = AppUserChangeForm
     model = AppUser
 
-    list_display = ['username', 'email', 'dni', 'account_status']
+    list_display = ['id','username', 'first_name', 'email', 'dni', 'account_status']
 
     fieldsets = BaseUserAdmin.fieldsets + (
         (None, {
@@ -26,10 +26,21 @@ class AppUserAdmin(BaseUserAdmin):
         }),
     )
     
-    search_fields = ('username', 'email', 'dni', 'account_status')
+    search_fields = ('username', 'first_name', 'last_name', 'email', 'dni', 'account_status')
     list_filter = ('account_status', 'is_staff', 'is_active', 'groups')
 
+class PatientAdmin(admin.ModelAdmin):
+    search_fields = ['user__email','user__username','user__first_name','user__last_name','user__dni', 'gender']
+    list_filter = ['gender']
+    list_display = ['id','user__username', 'user__first_name','user__last_name','user__email']
+
+class PhysioAdmin(admin.ModelAdmin):
+    search_fields = ['user__email','user__username','user__first_name','user__last_name','user__dni', 'gender',
+                     'autonomic_community', 'collegiate_number']
+    list_filter = ['gender', 'autonomic_community']
+    list_display = ['id','user__username', 'user__email', 'collegiate_number', 'autonomic_community']
+
 admin.site.register(AppUser, AppUserAdmin)
-admin.site.register(Patient)
-admin.site.register(Physiotherapist)
+admin.site.register(Patient, PatientAdmin)
+admin.site.register(Physiotherapist, PhysioAdmin)
 admin.site.register(Admin)
