561484ce884d65d53a8e3cbf6af4c9a03382c897
Ramón Gavira <91947011+rgavira123@users.noreply.github.com>
2025-03-27 15:50:43 +0100

    Update admin.py

--- backend/fisio_find/appointment/admin.py
+++ backend/fisio_find/appointment/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from appointment.models import Appointment
 
+class AppointmentAdmin(admin.ModelAdmin):
+    search_fields = [
+        'patient__user__email','patient__user__username','patient__user__first_name','patient__user__last_name','patient__user__dni',
+        'physiotherapist__user__email','physiotherapist__user__username','physiotherapist__user__first_name','physiotherapist__user__last_name','physiotherapist__user__dni',
+        ]
+    list_filter = ['is_online', 'start_time','end_time', 'status']
+    list_display = ['id','patient__user__username', 'physiotherapist__user__username','is_online', 'start_time','end_time']
+
+
 # Register your models here.
-admin.site.register(Appointment)
+admin.site.register(Appointment, AppointmentAdmin)
